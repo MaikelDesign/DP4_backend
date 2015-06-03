294fafce371bbf706928f66dd9a567884121f889
Verbeek,Maikel M.A.M <295741@student.fontys.nl>
2015-06-03 15:24:16 +0200

    no message

---
+++ index.php
@@ -0,0 +1,54 @@
+<?php
+error_reporting(E_ALL);
+ini_set("display_errors", 1);
+
+session_start();
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1"> <!-- scale for different screen sizes -->
+    <title>Build your own city</title>
+    <link href="css/stylesheet.css" rel="stylesheet">
+    <link href="css/queries.css" rel="stylesheet">
+<!--		<link rel="icon" type="image/png" href="images/icon.png"/> icon in url bar -->
+    <script src="js/jquery_v1.11.1.js"></script>
+    <script src="js/jquery-ui-1.10.4.min.js"></script>
+	  <script src="js/js.js"></script>
+    <script src="js/script.js"></script>
+   </head>
+   <body >
+    <div class="grid">
+    <div class="grid-item"><div class="circle"><a href='#' class='profile'><img src="img/profiel1.jpg" class="circle"></a></div></div>
+    <div class="grid-item grid-item--width2 grid-item--height2"></div>
+    <div class="grid-item grid-item--height3"></div>
+    <div class="grid-item grid-item--height2"></div>
+    <div class="grid-item grid-item--width3"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item grid-item--height2"></div>
+    <div class="grid-item grid-item--width2 grid-item--height3"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item grid-item--height2"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item grid-item--width2 grid-item--height2"></div>
+    <div class="grid-item grid-item--width2"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item grid-item--height2"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item grid-item--height3"></div>
+    <div class="grid-item grid-item--height2"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item"></div>
+    <div class="grid-item grid-item--height2"></div>
+    </div>
+   </body>
+</html>
+
+
